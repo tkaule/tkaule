e7651a3c514e7d2ceada9bb75508545ddabb5a30
tkaule <thomas.kaule@mitel.com>
2021-11-09 16:44:39 +0100

    Changed app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port3 = 3000;
-const port4 = 4000;
+const port5 = 5000;
 
 const server3 = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,7 +10,7 @@ const server3 = http.createServer((req, res) => {
   res.end('Hello World, my first server even works harder and harder never');
 });
 
-const server4 = http.createServer((req, res) => {
+const server5 = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World, I\'m the back side');
@@ -21,6 +21,6 @@ server3.listen(port3, hostname, () => {
   console.log(`Server running at http://${hostname}:${port3}/`);
 });
 
-server4.listen(port4, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port4}/`);
+server5.listen(port5, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port5}/`);
   });
